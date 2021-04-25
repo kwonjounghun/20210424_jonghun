43b1b1c1908450a2040a49651f23501952b90a49
kwonjounghun <kjh0102321@gmail.com>
2021-04-25 14:00:02 +0900

    feat : Thumbnail component 생성

---
+++ src/components/Thumbnail.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  photoUrl: string;
+};
+
+const Thumbnail: React.FC<Props> = ({
+  photoUrl,
+}) => {
+  return (
+    <ImageContainer>
+      <ImageBox photoUrl={photoUrl} data-testid="thumbnail"/>
+      이미지
+    </ImageContainer>
+  );
+};
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 269px;
+  height: 269px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  overflow: hidden;
+`;
+
+const ImageBox = styled.div<{ photoUrl: string }>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-size: cover;
+  background-position: center;
+  background-color: rgba(128, 128, 128, 0.192);
+  ${({ photoUrl }) => `background-image: url(${photoUrl});`}
+`;
+
+export default Thumbnail;
