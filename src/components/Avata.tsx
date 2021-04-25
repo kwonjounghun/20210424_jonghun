1a9044608e67d4b2b7f38a6d2cc5a9a3d19a057c
kwonjounghun <kjh0102321@gmail.com>
2021-04-25 13:58:02 +0900

    feat : Avata component 생성

---
+++ src/components/Avata.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import DefaultImage from '../assets/userDefault.svg';
+import styled from 'styled-components';
+
+interface Props {
+  photoUrl?: string;
+};
+
+const Avata: React.FC<Props> = ({
+  photoUrl,
+}) => {
+  return (
+    <ImageContainer>
+      <HiddenText>프로필 이미지</HiddenText>
+      <ImageBox src={photoUrl || DefaultImage} data-testid="Avata" />
+    </ImageContainer>
+  );
+};
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  overflow: hidden;
+`;
+
+const ImageBox = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const HiddenText = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0,0,0,0);
+`;
+
+export default Avata;
