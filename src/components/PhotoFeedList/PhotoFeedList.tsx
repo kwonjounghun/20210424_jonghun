a1d5d5296bb9ef4a4ce063c2090ae3ce8ad62ac6
kwonjounghun <kjh0102321@gmail.com>
2021-04-25 23:53:01 +0900

    feat : PhotoFeedList component 생성

---
+++ src/components/PhotoFeedList/PhotoFeedList.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PhotoFeedCard from '../PhotoFeedCard';
+import styled from 'styled-components';
+
+interface Props {
+  photoFeedList: Array<any>;
+};
+
+const PhotoFeedList: React.FC<Props> = ({
+  photoFeedList = [],
+  ...props
+}) => {
+  return (
+    <ListContainer>
+      {photoFeedList.map(({
+        id,
+        nickname,
+        image_url,
+        profile_image_url,
+      }) => {
+        return (
+          <ListItem key={id} data-testid="photo-feed-item">
+            <PhotoFeedCard
+              id={id}
+              photoUrl={image_url}
+              profileImgUrl={profile_image_url}
+              nickname={nickname}
+            />
+          </ListItem>
+        );
+      })}
+    </ListContainer>
+  );
+};
+
+const ListContainer = styled.ul`
+  padding: 0 60px;
+  display: inline-block;
+`;
+
+const ListItem = styled.li`
+  display: inline-block;
+  margin: 30px 10px 0;
+`;
+
+export default PhotoFeedList;
