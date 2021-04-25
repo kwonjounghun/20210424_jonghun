810a2dc1c0f62395f50dc3e96f050c54f67692f6
kwonjounghun <kjh0102321@gmail.com>
2021-04-25 13:53:40 +0900

    modify: storybook의 preview.js에 GlobalStyle 추가
    - storybook에서도 GlobalStyle을 사용할 수 있도록 decorators에 GlobalStyle.tsx 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,23 @@
+import GlobalStyle from '../src/GlobalStyle';
+
+const customViewports = {
+  desktop: {
+    name: 'Desktop',
+    type: 'desktop',
+    styles: {
+      width: '1024px',
+      height: '768px',
+    },
+  },
+  mobile: {
+    name: 'Mobile',
+    type: 'mobile',
+    styles: {
+      width: '360px',
+      height: '640px',
+    },
+  },
+};
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,4 +27,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyle />
+      {Story()}
+    </>
+  ),
+];
