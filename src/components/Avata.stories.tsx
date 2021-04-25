b017bd870ed83b22b4a25ed5f6985320ba7142fe
kwonjounghun <kjh0102321@gmail.com>
2021-04-25 13:58:25 +0900

    feat : Avata stories 생성

---
+++ src/components/Avata.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Avata from './Avata';
+
+export default {
+  title: 'Avata',
+  component: Avata,
+};
+
+const photoUrl = 'https://image.ohou.se/image/central_crop/bucketplace-v2-development/uploads-cards-snapshots1547907139_Oc9.jpeg/640/640';
+
+export const BasicUsage = (args: any) => <Avata {...args} />;
+
+BasicUsage.args = {
+  photoUrl,
+}
